55b10b83bd94188bfab4c4dec5e4ae37444a55d7
MSaadSheikh <95358942+MSaadSheikh@users.noreply.github.com>
2022-02-07 04:07:11 -0800

    Add files via upload

---
+++ assignment2.dart
@@ -0,0 +1,65 @@
+
+import 'dart:io';
+void main() {
+  // Program1
+
+// stdout.write("Add number:");
+// int dig=int.parse(stdin.readLineSync()!);
+// var total= (dig+4)*12/2;
+// print(total);
+
+// Program2
+
+// stdout.write("Add number:");
+// int dig=int.parse(stdin.readLineSync()!);
+// var total= (dig+4)*12/2;
+// print(total);
+
+// Program3
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+// var p1= --a;
+// var p2= --a - --b;
+// var p3= --a - --b + ++b;
+// var p4= --a - --b + ++b + b--;
+// print(result);
+// print(p1);
+// print(p2);
+// print(p3);
+// print(p4);
+
+// Program4
+
+//  String s="SMIT";
+//  String s2="SMIT";
+//  print(s.compareTo(s2));
+
+// Program5
+
+// String data="DELL";
+// int s=data.length;
+// print(s); 
+
+// Program6
+
+// String data = 'AbBTE';
+// int s = data.codeUnitAt(1);
+// print(s);
+
+// Program7
+
+// stdout.write("Write Anything:");
+// String get = stdin.readLineSync()!;
+// var check = get.substring(4,7);
+// print("$check");
+
+// Program8
+
+// String data= "ZEBTECH";
+// var var2=data.split("");
+// print(var2);
+
+
+// }
